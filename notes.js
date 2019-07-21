b426a86058b0ab3a139edd8be3bf2d020e2c1439
anuragcp <anudeveloper777@gmail.com>
2019-07-21 13:07:36 +0530

    solved error due to double assignment of immutable const datatype

--- notes.js
+++ notes.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 const addNote = function(title, body){
 	const note = loadNote();
-	console.log("loaded note : "+ note);
+	
 	note.push({
 		title : title,
 		body : body
@@ -12,13 +12,15 @@ const addNote = function(title, body){
 
 const loadNote = function(){
 	try {
-		const noteJSON = fs.readFileSync("notes.json")
-		noteJSON = noteJSON.toString()
-		console.log(noteJSON)
+		const dataBuffer = fs.readFileSync("notes.json");
+		const noteJSON = dataBuffer.toString();
+		console.log("loaded note : "+ noteJSON);
 		return JSON.parse(noteJSON)
-	} catch(e) {
+	}catch(e) {
+		console.log("Still executing here...")
 		return []
 	}
+	
 }
 
 const saveNote = function(note){
